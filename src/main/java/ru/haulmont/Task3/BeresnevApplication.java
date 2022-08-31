af2bb4a09228205577d69196676ea1cf55ccb2ab
beresnevOCRV <112411613+beresnevOCRV@users.noreply.github.com>
2022-08-31 07:56:17 +0300

    Add files via upload

---
+++ src/main/java/ru/haulmont/Task3/BeresnevApplication.java
@@ -0,0 +1,13 @@
+package ru.haulmont.Task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeresnevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeresnevApplication.class, args);
+	}
+
+}
